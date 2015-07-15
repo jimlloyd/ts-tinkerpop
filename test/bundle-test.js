ed436e278d2085362fe5708de0b01be7422f04e5
Jim Lloyd <jim.lloyd@gmail.com>
2015-07-15 14:09:03 -0700

    WIP

--- test/bundle-test.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../lib/index.d.ts"/>
-/// <reference path="../typings/mocha/mocha.d.ts"/>
-describe('bundle-test', function () {
-    it('compiles', function () {
-        // empty
-    });
-});
-//# sourceMappingURL=bundle-test.js.map
--- test/header-test.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../typings/ts-tinkerpop/index.d.ts"/>
-/// <reference path="../typings/mocha/mocha.d.ts"/>
-describe('header-test', function () {
-    it('compiles', function () {
-        // empty
-    });
-});
-//# sourceMappingURL=header-test.js.map
